bfcb68875e4f10fb4a9efd46a53aa4fe10bd6211
time_to_stop <=>
2025-01-07 16:35:56 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://timetostop.github.io/HTML_Academy_City_rent/',
   test: {
     globals: true,
     environment: 'jsdom',
