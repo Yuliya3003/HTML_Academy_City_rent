9e1f3af7f9882d3b6224885e458cdda8a9a4d00b
time_to_stop <=>
2025-01-07 19:29:39 +0300

    fix1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,9 +7,26 @@ import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import Error404 from '../../pages/Error404/Error404';
 import useAppInit from '../../utils/use-init-app/use-init-app';
 import { APP_ROUTES } from '../../services/constants';
+import { useEffect } from 'react';
 
 function App(): JSX.Element {
   useAppInit();
+  useEffect(() => {
+    setTimeout(() => {
+      // yandex metrika 
+      var counter = (window as any).yaCounter99437467;
+    
+      if (counter && counter.getClientID) {
+        counter.getClientID().then((clientID: number) => {
+          console.log("Client ID текущего пользователя:", clientID);
+        }).catch((error: string) => {
+          console.error("Ошибка получения Client ID:", error);
+        });
+      } else {
+        console.error("Счетчик Яндекс Метрики не найден или не загружен.");
+      }
+    }, 5000);    
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-setTimeout(() => {
-  // yandex metrika 
-  var counter = (window as any).yaCounter99437467;
-
-  if (counter && counter.getClientID) {
-    counter.getClientID().then((clientID: number) => {
-      console.log("Client ID текущего пользователя:", clientID);
-    }).catch((error: string) => {
-      console.error("Ошибка получения Client ID:", error);
-    });
-  } else {
-    console.error("Счетчик Яндекс Метрики не найден или не загружен.");
-  }
-}, 5000);
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
