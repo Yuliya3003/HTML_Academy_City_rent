eb5c90615b3738dd2330d5a58d15a0662142f9d4
time_to_stop <=>
2025-01-07 19:24:20 +0300

    add client id

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+// yandex metrika 
+var counter = (window as any).yaCounter99437467;
+
+if (counter && counter.getClientID) {
+    counter.getClientID().then((clientID: number) => {
+        console.log("Client ID текущего пользователя:", clientID);
+    }).catch((error: string) => {
+        console.error("Ошибка получения Client ID:", error);
+    });
+} else {
+    console.error("Счетчик Яндекс Метрики не найден или не загружен.");
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
